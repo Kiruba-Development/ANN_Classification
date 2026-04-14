55c36f4feb2753f371f38aaabea836cff5342627
sj kiruba <95710513+Sjkiruba03@users.noreply.github.com>
2026-04-14 14:03:48 +0530

    streamlit option ANN

---
+++ streamlit_regression.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import tensorflow as tf
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+import pickle
+import pandas as pd
+
+
+## Load the model
+model = tf.keras.models.load_model('ANNs_project/regressionmodel.h5')
+
+## Load the encoders 
+
+with open('ANNs_project/label_encoder_gender.pkl','rb') as file:
+    label_encoder_gender = pickle.load(file)
+
+with open('ANNs_project/onehot_encoder_geo.pkl','rb') as file:
+    onehot_encoder_geo = pickle.load(file)
+
+with open('ANNs_project/scaler_salary.pkl', 'rb') as file:
+    scaler = pickle.load(file)
+
+
+
+
+## Streamlit App
+
+st.title("Customer Chrun Prediction")
+
+geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
+gender = st.selectbox('Gender', label_encoder_gender.classes_)
+age = st.slider('Age', 18, 92)
+balance = st.number_input('Balance')
+credit_score = st.number_input('Credit Score')
+exited = st.selectbox("Exited", [0,1])
+tenure = st.slider('Tenure', 0,10)
+num_of_products = st.slider('Number Of Products',1,4)
+has_cr_card = st.selectbox('Has Credit Card',[0,1])
+is_active_member = st.selectbox('IsActiveMember', [0,1])
+
+
+## Prepare the input data
+
+input_data = pd.DataFrame({
+     'CreditScore': [credit_score],
+    'Gender': [label_encoder_gender.transform([gender])[0]],
+    'Age': [age],
+    'Tenure': [tenure],
+    'Balance': [balance],
+    'NumOfProducts': [num_of_products],
+    'HasCrCard': [has_cr_card],
+    'IsActiveMember': [is_active_member],
+    'Exited': [exited]
+})
+
+## One hot encoding for geography
+
+geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
+geo_encoder_df = pd.DataFrame(geo_encoded,columns= onehot_encoder_geo.get_feature_names_out  (['Geography']))
+
+## Combine the one hot encode with input data
+
+input_data = pd.concat([input_data.reset_index(drop=True), geo_encoder_df], axis=1)
+
+## Scale the input data
+
+input_data_scaled = scaler.transform(input_data)
+
+
+## Predict Churn
+prediction = model.predict(input_data_scaled)
+prediction_salary = prediction[0][0]
+
+print(prediction)
+
+st.write(f'Estimated Salary: {prediction_salary:.2f}')
+
+# st.write(f'Chrun Probability: {prediction_prob:.2f}')
+
+
+
